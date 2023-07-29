47ecf2429bd480ce085ab260979aff9ca0e81451
kaviarasu <kaviarasu@qmaxsys.com>
2023-07-29 13:37:30 +0530

    di initial commit

---
+++ di/src/computer/computer.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ComputerController } from './computer.controller';
+import { CpuModule } from '../cpu/cpu.module';
+import { DiskModule } from '../disk/disk.module';
+
+
+
+@Module({
+  providers: [],
+  controllers: [ComputerController],
+  imports: [ CpuModule, DiskModule ]
+})
+export class ComputerModule {}
